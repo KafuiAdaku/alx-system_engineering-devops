9cd4ec9aa3a97df9da1c08e31e756a6c4b1b3b5b
Your Name <you@example.com>
2023-11-11 09:11:05 +0000

    fixed lint errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'fix_wp_settings':
- command => 'sed -i "s/\.phpp/\.php/g" /var/www/html/wp-settings.php',
- path   => '/usr/bin:/bin',
+  command => 'sed -i "s/\.phpp/\.php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/bin',
 }
